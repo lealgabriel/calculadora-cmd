3aa05bab55a8df9408bfb33f21ec43be3ec34b32
Gabriel Leal <gabrielleal96.gl@gmail.com>
2021-12-14 15:38:51 -0400

    Projeto iniciado - Menu criado

---
+++ Calculadora_CMD/Enums/Menu.cs
@@ -0,0 +1,13 @@
+﻿namespace Calculadora_CMD.Enums
+{
+    enum Menu : int
+    {
+        Soma = 1,
+        Subtracao = 2,
+        Divisao = 3,
+        Multiplicacao = 4,
+        Potencia = 5,
+        Raiz = 6,
+        Sair = 7
+    }
+}
---
+++ Calculadora_CMD/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Calculadora_CMD.Enums;
+
+namespace Calculadora_CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bem vindo ao CALC! Selecione uma das opções: ");
+            Console.WriteLine();
+            Console.WriteLine("1 - Soma\n2 - Subtração\n3 - Divisão\n4 - Multiplicação\n5 - Potência\n6 - Raiz\n7 - Sair");
+            Console.ReadLine();
+
+        }
+    }
+
+
+}
