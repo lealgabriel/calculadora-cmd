5cc94e7b7acbeca5fa5fcda5a52e398ed917832b
Gabriel Leal <gabrielleal96.gl@gmail.com>
2021-12-14 18:12:19 -0400

    Função de SOMA adicionada

---
+++ Calculadora_CMD/Operacoes/Soma.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculadora_CMD.Operacoes
+{
+    class Soma
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public int resultado; 
+
+        public Soma(int a, int b)
+        {
+            A = a;
+            B = b;
+
+            resultado = a + b;
+        }
+
+       
+
+    }
+}
--- Calculadora_CMD/Program.cs
+++ Calculadora_CMD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Calculadora_CMD.Enums;
+using Calculadora_CMD.Operacoes;
 
 namespace Calculadora_CMD
 {
@@ -9,6 +10,10 @@ namespace Calculadora_CMD
         static void Main(string[] args)
         {
             Menu opcao = new Menu();
+            Soma soma;
+            
+
+            
 
             bool escolheuSair = false;
             while (!escolheuSair)
@@ -21,6 +26,26 @@ namespace Calculadora_CMD
 
                 switch (opcao)
                 {
+                    case Menu.Soma:
+                        Console.WriteLine("Soma de dois números:");
+                        Console.WriteLine();
+                        Console.WriteLine("Digite o primeiro número: ");
+                        Console.WriteLine();
+                         int n1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine();
+                        Console.WriteLine("Digite o segundo número: ");
+                        Console.WriteLine();
+                        int n2 = int.Parse(Console.ReadLine());
+
+                        soma = new Soma(n1, n2);
+                        Console.WriteLine();
+                        Console.WriteLine($"O resultado é {soma.resultado}");
+                        Console.WriteLine();
+                        Console.WriteLine("Aperte ENTER para voltar ao menu");
+                        Console.ReadLine();
+                        
+                        break;
+
                     case Menu.Sair:
                         escolheuSair=true;
                         break;
